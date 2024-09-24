56deedc287fc903d1118f0d9746c32c7be5d00d9
nakaay13 <karbrz01@easv365.dk>
2024-09-24 19:59:49 +0200

    some images changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,28 +8,28 @@
   </div>
   <div class="row light-blue g-0 d-flex justify-content-center align-items-center"> 
     <div class="card me-3 text-primary bg-secondary rounded-4 text-center " style="width: 17rem;">
-        <img src="../img/vape1.png" class="card-img-top rounded-top-4" alt="...">
+        <img src="../img/matcha-1.jpg" class="card-img-top rounded-top-4" alt="...">
         <div class="card-body">
           <h5 class="card-title">VAPOR  VAPE Q133 MINI BLACK</h5>
           <p class="card-text">300 DKK</p>
         </div>
     </div>
     <div class="card me-3 text-primary bg-secondary rounded-4 text-center" style="width: 17rem;">
-        <img src="../img/vape2.png" class="card-img-top rounded-top-4" alt="...">
+        <img src="../img/matcha-1.jpg" class="card-img-top rounded-top-4" alt="...">
         <div class="card-body">
           <h5 class="card-title">VAPOR  VAPE D176 STANDARD SILVER</h5>
           <p class="card-text">300 DKK</p>
         </div>
     </div>
     <div class="card me-3 text-primary bg-secondary rounded-4 text-center" style="width: 17rem;">
-        <img src="../img/vape3.png" class="card-img-top rounded-top-4" alt="...">
+        <img src="../img/matcha-1.jpg" class="card-img-top rounded-top-4" alt="...">
         <div class="card-body">
           <h5 class="card-title">VAPOR VAPE H435 ECO BLACK</h5>
           <p class="card-text">350 DKK</p>
         </div>
     </div>
     <div class="card last-card text-primary bg-secondary rounded-4 text-center" style="width: 17rem;">
-        <img src="../img/vape4.png" class="card-img-top rounded-top-4" alt="...">
+        <img src="../img/matcha-1.jpg" class="card-img-top rounded-top-4" alt="...">
         <div class="card-body">
           <h5 class="card-title">VAPOR VAPE G769 PRO SILVER</h5>
           <p class="card-text">400 DKK</p>
